9cbbdad6653dede5a269f4eb2cacc2f2882bf5b1
DiegoFlores18 <36686852+DiegoFlores18@users.noreply.github.com>
2021-01-31 23:57:25 -0600

    New parser tests.
    Adding new parser tests for 3rd delivery. (Unary operators)

--- alphax_compiler/test/parser_test.exs
+++ alphax_compiler/test/parser_test.exs
@@ -141,7 +141,7 @@ defmodule ParserTest do
 
   test "Missing return value" do
     ast= Lexer.lexing(File.read!("test/pruebas/missing_return.c"))
-    assert Parser.parse_program(ast) ==  {:error, "ERROR AT 2: expect an int value"}
+    assert Parser.parse_program(ast) ==  {:error, "ERROR AT 2: expected an int value"}
   end
 
   test "no brace" do
@@ -165,5 +165,222 @@ defmodule ParserTest do
 
   end
 
+  #Unary operators (to pass)
 
+  test "neg" do
+    ast = Lexer.lexing(File.read!("test/pruebas/neg.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: %AST{
+                left_node: nil,
+                node_name: :constant,
+                right_node: nil,
+                value: 5
+              },
+              node_name: :unary,
+              right_node: nil,
+              value: :negation
+            },
+            node_name: :return,
+            right_node: nil,
+            value: :return
+          },
+          node_name: :function,
+          right_node: nil,
+          value: :main
+        },
+        node_name: :program,
+        right_node: nil,
+        value: nil
+    }
+  end
+
+  test "bitW" do
+    ast = Lexer.lexing(File.read!("test/pruebas/bitwise.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 12
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :bitW,
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "logicalNeg" do
+    ast = Lexer.lexing(File.read!("test/pruebas/not_five.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 5
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalN
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "not_zero" do
+    ast = Lexer.lexing(File.read!("test/pruebas/not_zero.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: nil,
+              node_name: :constant,
+              right_node: nil,
+              value: 0
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalN
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return,
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main,
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "nested_operators" do
+    ast = Lexer.lexing(File.read!("test/pruebas/nested_ops.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: %AST{
+                left_node: nil,
+                node_name: :constant,
+                right_node: nil,
+                value: 3
+              },
+              node_name: :unary,
+              right_node: nil,
+              value: :negation
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :logicalN
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  test "nested_operators_2" do
+    ast = Lexer.lexing(File.read!("test/pruebas/nested_ops_2.c"))
+    assert Parser.parse_program(ast) ==
+    %AST{
+      left_node: %AST{
+        left_node: %AST{
+          left_node: %AST{
+            left_node: %AST{
+              left_node: %AST{
+                left_node: nil,
+                node_name: :constant,
+                right_node: nil,
+                value: 0
+              },
+              node_name: :unary,
+              right_node: nil,
+              value: :bitW
+            },
+            node_name: :unary,
+            right_node: nil,
+            value: :negation
+          },
+          node_name: :return,
+          right_node: nil,
+          value: :return
+        },
+        node_name: :function,
+        right_node: nil,
+        value: :main
+      },
+      node_name: :program,
+      right_node: nil,
+      value: nil
+    }
+  end
+
+  #unary operators (test to fail)
+  test "wrong_order" do
+    ast = Lexer.lexing(File.read!("test/pruebas/wrong_order.c"))
+
+    assert Parser.parse_program(ast) == {:error, "ERROR AT 2: expected an int value"}
+  end
+
+  test "semicolon_missed" do
+    ast = Lexer.lexing(File.read!("test/pruebas/missing_semicolon.c"))
+    assert Parser.parse_program(ast) == {:error, "ERROR AT 3: semicolon missed after constant to finish return statement "}
+  end
+
+  test "const_missed" do
+    ast = Lexer.lexing(File.read!("test/pruebas/missing_const.c"))
+    assert Parser.parse_program(ast) == {:error, "ERROR AT 2: expected an int value"}
+  end
+  test "constant_missed/nested_ops" do
+    ast = Lexer.lexing(File.read!("test/pruebas/nested_missing_const.c"))
+    assert Parser.parse_program(ast) == {:error, "ERROR AT 2: expected an int value"}
+  end
 end
