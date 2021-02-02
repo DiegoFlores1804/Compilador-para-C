a95a7cff459834c3050463d15b41b19a2f7133d7
DiegoFlores18 <36686852+DiegoFlores18@users.noreply.github.com>
2021-02-02 04:33:26 -0600

    Phase II & III
    This updated version of the code generator was developed as a functional requirement for this two deliveries were unary and binary operators shall be possible to process by the compiler.

--- alphax_compiler/lib/CodeGenerator.ex
+++ alphax_compiler/lib/CodeGenerator.ex
@@ -1,21 +1,34 @@
 defmodule CodeGenerator do
 
   def generate_code(ast) do
-    code = post_order(ast) #postorder traversal
+    code = post_order(ast, " ") #postorder traversal
     code
   end
 
   #posorder travesal at AST, cocatenates assembler code for each root node
-  def post_order(node) do
+  def post_order(node, code_snippet) do
     case node do
       nil ->                #When there isn't node
         ""
 
       ast_node ->
 
-        code_snippet = post_order(ast_node.left_node)   #first, left travel
-        post_order(ast_node.right_node)                 #right travel.
-        emit_code(ast_node.node_name, code_snippet, ast_node.value) #calls every function emite_code, depending on the node name.
+         if ast_node.node_name == :constant do
+           emit_code(:constant, code_snippet, ast_node.value)
+         else
+
+           emit_code(
+             ast_node.node_name,
+             post_order(ast_node.left_node, code_snippet) <>
+             pushOp(ast_node) <>
+             post_order(ast_node.right_node, code_snippet) <>
+             popOp(ast_node),
+             ast_node.value
+           )
+         end
+        #  code_snippet = post_order(ast_node.left_node)   #first, left travel
+        #  post_order(ast_node.right_node)                 #right travel.
+        #  emit_code(ast_node.node_name, code_snippet, ast_node.value) #calls every function emite_code, depending on the node name.
 
 
     end
@@ -39,17 +52,96 @@ defmodule CodeGenerator do
       code_snippet                           #concatenates with assembler code.
   end
 
-  #When first parameter is return node
+  #When fisrt parameter is return node
   def emit_code(:return, code_snippet, _) do
+    code_snippet <>
     """
-        movl    #{code_snippet}, %rax
         ret
     """
   end
 
   #When first parameter is constant node, this function will be called first.
   def emit_code(:constant, _code_snippet, value) do
-    "$#{value}"                            #take value and puts it in the assembler code.
+    """
+        movl   #{value}, %rax
+    """
   end
 
+  def emit_code(:unary, code_snippet, :negation) do
+    code_snippet <>
+    """
+        neg   %rax
+    """
+  end
+
+  def emit_code(:unary, code_snippet, :logicalN) do
+    code_snippet <>
+    """
+        cmpl   $0, %rax
+        movl   $0, %rax
+        sete   %al
+    """
+  end
+  def emit_code(:unary, code_snippet, :bitW) do
+    code_snippet <>
+    """
+        not    %rax
+    """
+  end
+
+  def emit_code(:binary, code_snippet, :addition) do
+    code_snippet <>
+    """
+        pop    %rax
+        add    %rax, %rcx
+    """
+  end
+
+  def emit_code(:binary, code_snippet, :negation) do
+    code_snippet <>
+    """
+        sub    %rax, %rbx
+        mov    %rbx, %rax
+    """
+  end
+
+  def emit_code(:binary, code_snippet, :multiplication) do
+    code_snippet <>
+    """
+        imul   %rbx, %rax
+    """
+  end
+
+  def emit_code(:binary, code_snippet, :division) do
+    code_snippet <>
+    """
+        push   %rax
+        mov    %rbx, %rax
+        pop    %rbx
+        cqo
+        idivq  %rbx
+    """
+  end
+
+   def pushOp(ast_node) do
+     if ast_node.node_name == :unary and ast_node.value == :negation and ast_node.right_node == nil do
+       """
+           _NEG
+       """
+     else
+       """
+           push   %rax
+       """
+     end
+   end
+
+   def popOp(ast_node) do
+     if ast_node.node_name == :unary and ast_node.value == :negation and ast_node.right_node == nil do
+       ""
+     else
+       """
+           pop   %rbx
+       """
+     end
+   end
 end
